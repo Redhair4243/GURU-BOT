f88499f262f05d23822de40c484aa2c808f9619b
Redhair4243 <165999303+Redhair4243@users.noreply.github.com>
2024-04-04 09:38:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['09153609744', 'Redhair', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
